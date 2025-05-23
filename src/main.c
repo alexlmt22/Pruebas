371fd4444fd6b20aaaac1e838243bd65a8b7c6e0
Alexisjavi777 <javiercayo25@gmail.com>
2025-05-23 17:30:18 -0500

    new try

--- src/main.c
+++ src/main.c
@@ -7,8 +7,8 @@ int suma (int a, int b)
 int main()
 {
     int s = 0;
-    printf("Hello World!\n");
-    s = suma(8,5);
+    printf("Hello World again!\n");
+    s = suma(10,5);
     printf("La suma es %d\n",s);
 
     return 0;
